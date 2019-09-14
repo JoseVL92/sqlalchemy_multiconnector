5cc946dadd7e9be92bd7e62dff90dd8c057a7fb6
Jose Alberto Varona Labrada <jovalab92@gmail.com>
2019-09-14 04:51:59 -0400

    Multiple engines
    Adding possibility of working with more than a single database

--- sqlalchemy_connector.py
+++ sqlalchemy_connector.py
@@ -1,55 +1,89 @@
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy import create_engine
 from contextlib import contextmanager
 from functools import wraps
+
+from sqlalchemy import create_engine
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
 
 BASE = declarative_base()
 
 
-def get_uri(db_type, db_host_or_path, db_port, db_name, db_user, db_passwd):
+def get_uris(db_type, db_host_or_path, db_port, db_name, db_user, db_passwd):
+    if not db_type or not db_host_or_path or not db_name:
+        raise ValueError("Not enough data")
     if db_type == "sqlite":
-        return f"sqlite:///{db_host_or_path}"
-    uri = f"{db_type}://"
-    if db_user:
-        uri += db_user
-        if db_passwd:
-            uri += ":" + db_passwd
-        uri += "@"
-    uri += db_host_or_path
-    if db_port:
-        uri += ":" + str(db_port)
-    if db_name:
-        uri += "/" + db_name
-    return uri
+        # ensure that no trailing '/' is present
+        if db_host_or_path[-1] == '/':
+            db_host_or_path = db_host_or_path[:-1]
+        uri = f"sqlite:///{db_host_or_path}"
+        # return {"default": f"sqlite:///{db_host_or_path}"}
+    else:
+        uri = f"{db_type}://"
+        if db_user:
+            uri += db_user
+            if db_passwd:
+                uri += ":" + db_passwd
+            uri += "@"
+        uri += db_host_or_path
+        if db_port:
+            uri += ":" + str(db_port)
+    # if db_name is a list and db name 'default' is not specified,
+    # the default database would be the first in the db_name list
+    if isinstance(db_name, list):
+        uri_dict = {name: uri + "/" + name for name in db_name}
+        if 'default' not in uri_dict:
+            uri_dict['default'] = uri + "/" + db_name[0]
+    else:
+        uri_dict = {'default': uri + "/" + db_name}
+    return uri_dict
 
 
 class SQLConnector:
-    def __init__(self, db_type, db_host_or_path, db_port=None, db_name=None, db_user=None, db_passwd=None):
+    def __init__(self, db_type, db_host_or_path, db_name, db_port=None, db_user=None, db_passwd=None):
+        """
+        Creates an object with necessary parameters for connecting to a sql database
+        :param db_type: One of 'sqlite', 'postgresql' or 'mysql'
+        :param db_host_or_path: If db_type=='sqlite', it is the absolute path of the folder containing the file, otherwise it is a hostname or ip
+        :param db_port: Port where db server is listening. None if db_type='sqlite'
+        :param db_name: If just one database will be used, it is a single db name (a file name if db_name='sqlite').
+               If multiple databases, it would be a list of db names or file names.
+        :param db_user: Db server login user. None if db_type='sqlite'
+        :param db_passwd: Db server login password. None if db_type='sqlite'
+        """
         allowed_types = ("sqlite", "postgresql", "mysql")
+        if not db_name:
+            raise AttributeError("Must specify at least one db_name")
         if db_type in allowed_types:
-            self.connection_uri = get_uri(db_type, db_host_or_path, db_port, db_name, db_user, db_passwd)
+            if db_type != 'sqlite' and not (db_name and db_user and db_passwd):
+                raise AttributeError(f"db_user and db_password must be declared for {db_type}")
+            self.connection_uris = get_uris(db_type, db_host_or_path, db_port, db_name, db_user, db_passwd)
         else:
             raise ValueError(f"{db_type} not in {str(allowed_types)}")
 
-        self.engine = create_engine(self.connection_uri)
-        self.Session = sessionmaker(bind=self.engine, autoflush=True)
+        self.engines = {name: create_engine(uri) for name, uri in self.connection_uris.items()}
+        self.Session = sessionmaker(autoflush=True)
 
     def create_tables(self):
-        # if not hasattr(self, 'BASE'):
-        #     raise AttributeError("You must create the variable 'BASE' in the class definition")
-        # self.BASE.metadata.create_all(self.engine)
-        BASE.metadata.create_all(self.engine)
+        for _, engine in self.engines.items():
+            BASE.metadata.create_all(engine)
 
-    def execute_query(self, query):
-        connection = self.engine.connect()
-        return connection.execute(query).fetch_all()
+    def execute_query(self, query, engine_name='default'):
+        engine = self.engines.get(engine_name)
+        if engine is None:
+            raise ValueError(f"No engine with name {engine_name}")
+        connection = engine.connect()
+        response = connection.execute(query).fetch_all()
+        connection.close()
+        return response
 
     @contextmanager
-    def session_scope(self):
+    def session_scope(self, engine_name='default'):
         """Provide a transactional scope around a series of operations."""
-        session = self.Session()
+        engine = self.engines.get(engine_name)
+        if engine is None:
+            raise ValueError(f"No engine with name {engine_name}")
+        session = self.Session(bind=engine)
         try:
             yield session
             session.commit()
@@ -67,8 +101,10 @@ def manage_session(function):
     @wraps(function)
     def manager(*args, **kwargs):
         if 'session' not in kwargs:
-            with args[0].session_scope() as session:
+            db_name = kwargs.get('db_name', 'default')
+            with args[0].session_scope(engine_name=db_name) as session:
                 kwargs.update({"session": session})
                 return function(*args, **kwargs)
         return function(*args, **kwargs)
+
     return manager
