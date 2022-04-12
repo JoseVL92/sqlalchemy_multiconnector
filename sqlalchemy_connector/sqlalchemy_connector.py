19b0cee3bd12acb3cebd54714da59630667fb31f
JoseVL <jovalab92@gmail.com>
2022-04-12 11:54:55 -0400

    Adding multi-schema functionalities.

--- sqlalchemy_connector/sqlalchemy_connector.py
+++ sqlalchemy_connector/sqlalchemy_connector.py
@@ -42,14 +42,16 @@ def get_uris(db_type, db_host_or_path, db_port, db_name, db_user, db_passwd):
 
 
 class SQLConnector:
-    def __init__(self, db_type, db_host_or_path, db_name, db_port=None, db_user=None, db_passwd=None):
+    def __init__(self, db_type, db_host_or_path, db_name, db_port=None, db_schemas=None, db_user=None, db_passwd=None,
+                 session_autoflush=True, session_autocommit=False):
         """
         Creates an object with necessary parameters for connecting to a sql database
         :param db_type: One of 'sqlite', 'postgresql' or 'mysql'
         :param db_host_or_path: If db_type=='sqlite', it is the absolute path of the folder containing the file, otherwise it is a hostname or ip
-        :param db_port: Port where db server is listening. None if db_type='sqlite'
         :param db_name: If just one database will be used, it is a single db name (a file name if db_name='sqlite').
                If multiple databases, it would be a list of db names or file names.
+        :param db_port: Port where db server is listening. None if db_type='sqlite'
+        :param db_schemas: List of schemas used on every specified database
         :param db_user: Db server login user. None if db_type='sqlite'
         :param db_passwd: Db server login password. None if db_type='sqlite'
         """
@@ -63,29 +65,70 @@ class SQLConnector:
         else:
             raise ValueError(f"{db_type} not in {str(allowed_types)}")
 
-        self.engines = {name: create_engine(uri) for name, uri in self.connection_uris.items()}
-        self.Session = sessionmaker(autoflush=True)
+        self.schemas = db_schemas if not db_type == 'sqlite' else None
+        if isinstance(self.schemas, str):
+            self.schemas = [self.schemas]
 
-    def create_tables(self):
+        self.engines = {
+            name: create_engine(uri) for name, uri in self.connection_uris.items()
+        }
+        self.Session = sessionmaker(autoflush=session_autoflush, autocommit=session_autocommit)
+
+    def create_tables(self, schemas=None):
+        schemas = schemas or self.schemas
+        if isinstance(schemas, str):
+            schemas = [schemas]
+        self._create_schemas(schemas)
         for _, engine in self.engines.items():
-            BASE.metadata.create_all(engine)
+            if schemas is not None:
+                for sc in self.schemas:
+                    BASE.metadata.create_all(
+                        bind=engine.connect().execution_options(
+                            schema_translate_map={None: sc}
+                        )
+                    )
+            else:
+                BASE.metadata.create_all(engine)
+
+    def _create_schemas(self, schemas=None):
+        schemas = schemas or self.schemas
+        if schemas is None:
+            return
+        if isinstance(schemas, str):
+            schemas = [schemas]
+        for engine_name, _ in self.engines.items():
+            for sc in schemas:
+                self.execute_query("CREATE SCHEMA IF NOT EXISTS " + sc, engine_name)
 
     def execute_query(self, query, engine_name='default'):
+        """Execute a raw query on database 'engine_name'.
+        If any schema will be used, it must be specified in the sql statement"""
         engine = self.engines.get(engine_name)
         if engine is None:
             raise ValueError(f"No engine with name {engine_name}")
         connection = engine.connect()
-        response = connection.execute(query).fetch_all()
+        response = connection.execute(query)
+        returnable = False
+        if hasattr(response, '.fetch_all()'):
+            response = response.fetch_all()
+            returnable = True
         connection.close()
-        return response
+        if returnable:
+            return response
 
     @contextmanager
-    def session_scope(self, engine_name='default'):
+    def session_scope(self, engine_name='default', schema_name=None):
         """Provide a transactional scope around a series of operations."""
         engine = self.engines.get(engine_name)
         if engine is None:
             raise ValueError(f"No engine with name {engine_name}")
-        session = self.Session(bind=engine)
+        if schema_name:
+            connection = engine.connect().execution_options(
+                schema_translate_map={None: schema_name}
+            )
+            session = self.Session(bind=connection)
+        else:
+            session = self.Session(bind=engine)
         try:
             yield session
             session.commit()
@@ -108,7 +151,8 @@ def manage_session(function):
     def manager(*args, **kwargs):
         if 'session' not in kwargs:
             db_name = kwargs.get('db_name', 'default')
-            with args[0].session_scope(engine_name=db_name) as session:
+            schema_name = kwargs.get('schema_name')
+            with args[0].session_scope(engine_name=db_name, schema_name=schema_name) as session:
                 kwargs.update({"session": session})
                 return function(*args, **kwargs)
         return function(*args, **kwargs)
